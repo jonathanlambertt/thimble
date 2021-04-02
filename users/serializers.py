78c133c5bed26b9e1baae24841729a49b6e4f943
Patrick <patrickmucsi@aol.com>
2021-04-01 22:13:50 -0700

    Basic profile creation is completed

---
+++ users/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+
+from django.contrib.auth.models import User
+
+from .models import Profile
+
+import uuid
+
+class CreateProfileSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(min_length=3, max_length=50)
+    password = serializers.CharField(min_length=6)
+    full_name = serializers.CharField(max_length=100)
+
+    class Meta:
+        model = User
+        fields = ['full_name','username','password','email']
+
+    # Check if username is taken
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("Username is already in use")
+        else:
+            return value
+
+    # Check if email is taken
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email is already in use")
+        else:
+            return value
+
+    # Create new user, profile and auth token
+    def create(self, validated_data):
+        new_user = User.objects.create_user(username=validated_data['username'],password=validated_data['password'],email=validated_data['email'])
+        new_profile = Profile.objects.create(user=new_user, full_name=validated_data['full_name'], uuid=uuid.uuid4())
+        Token.objects.create(user=new_user)
+        return new_profile
--- users/views.py
+++ users/views.py
@@ -3,10 +3,13 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 
+from .serializers import *
+
 # Register a new user
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def register(request):
+    cps = CreateProfileSerializer(data=request.data)
+    if cps.is_valid(raise_exception=True):
+        cps.save()
     return Response('good')
-
-
