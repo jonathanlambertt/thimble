df8251979f70e00dff797a8c074837a69d1ab97f
Patrick <patrickmucsi@aol.com>
2021-05-17 22:21:15 -0700

    Fixed creator being returned with joined groups

--- groups/views.py
+++ groups/views.py
@@ -21,7 +21,7 @@ def group_view(request, group_type):
     current_profile = Profile.get_profile(request.user)
     group_types = {'joined':current_profile.joined_groups, 'created':current_profile.my_groups}
     if group_type in group_types:
-        serialized_groups = GroupViewSerializer(group_types[group_type].all(), many=True)
+        serialized_groups = GroupViewSerializer(group_types[group_type].all().exclude(creator=current_profile), many=True)
         return Response(serialized_groups.data)
 
 @api_view(['GET'])
