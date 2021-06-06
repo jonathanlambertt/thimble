a0b508787dbd656feac44192c5d9cf3cc645d77d
Jonathan <jonathanlambert247@gmail.com>
2021-06-05 21:13:00 -0700

    changed code that has getting groups by old method

--- groups/views.py
+++ groups/views.py
@@ -36,7 +36,7 @@ def group_view(request, group_type):
 
 @api_view(['GET'])
 def list_members(request, group_id, option):
-    current_group_members = Group.get_group_by_uuid(group_id).members
+    current_group_members = Group.get_by_uuid(group_id).members
     options = {'all': current_group_members.all(), 
                 'removable':current_group_members.all().exclude(uuid=Profile.get_profile(request.user).uuid)}
     serialized_data = FriendsListResultSerializer(options[option], many=True)
@@ -48,7 +48,7 @@ def leave_group(request, group_id):
 
 @api_view(['PUT'])
 def perform_action(request, group_id, action, profile_id):
-    current_group = Group.get_group_by_uuid(group_id)
+    current_group = Group.get_by_uuid(group_id)
     possible_actions = {'add':current_group.add_member, 'remove':current_group.remove_member}
     if action in possible_actions:
         possible_actions[action](Profile.get_by_uuid(profile_id))
@@ -58,13 +58,13 @@ def perform_action(request, group_id, action, profile_id):
 @api_view(['GET'])
 def user_status(request, group_id):    
     result = {'message':'member'}
-    if Group.get_group_by_uuid(group_id).creator == Profile.get_profile(request.user):
+    if Group.get_by_uuid(group_id).creator == Profile.get_profile(request.user):
         result['message'] = 'owner'
     return Response(result)
         
 @api_view(['GET'])
 def potential_members(request, group_id):
-    the_group = Group.get_group_by_uuid(group_id)
+    the_group = Group.get_by_uuid(group_id)
     you = Profile.get_profile(request.user)
     possible_members = you.friends.exclude(uuid__in=[member.uuid for member in the_group.members.all()])
     return Response(FriendsListResultSerializer(possible_members, many=True).data)
@@ -72,7 +72,7 @@ def potential_members(request, group_id):
 @api_view(['GET'])
 def posts(request, group_id):
     post_serializers = {0:TextPostSerializer, 1: LinkPostSerializer, 2: PhotoPostSerializer}
-    cur = Group.get_group_by_uuid(group_id)
+    cur = Group.get_by_uuid(group_id)
     info = []
     for post in cur.posts.all().order_by("-timestamp"):
         info.append(post_serializers[post.post_type](post).data)
--- posts/views.py
+++ posts/views.py
@@ -19,6 +19,6 @@ def create_post(request):
         if request.data['post_type'] in post_serializers:
             post_serializer = post_serializers[request.data['post_type']](data=request.data)
             if post_serializer.is_valid(raise_exception=True):
-                post_serializer.save(owner=Profile.get_profile(request.user), group=Group.get_group_by_uuid(request.data['group']), uuid=uuid.uuid4())
+                post_serializer.save(owner=Profile.get_profile(request.user), group=Group.get_by_uuid(request.data['group']), uuid=uuid.uuid4())
 
     return Response(status=status.HTTP_201_CREATED)
