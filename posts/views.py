f9b222546e489291053cb7e1b102959e10b68282
Jonathan <jonathanlambert247@gmail.com>
2021-06-15 15:27:24 -0700

    test 2

--- posts/views.py
+++ posts/views.py
@@ -21,13 +21,9 @@ def create_post(request):
         if request.data['post_type'] in post_serializers:
             post_serializer = post_serializers[request.data['post_type']](data=request.data)
             if post_serializer.is_valid(raise_exception=True):
-<<<<<<< HEAD
-                post_serializer.save(owner=Profile.get_profile(request.user), group=Group.get_by_uuid(request.data['group']), uuid=uuid.uuid4())
-=======
                 post_uuid = uuid.uuid4()
                 current_group = Group.get_by_uuid(request.data['group'])
                 post_serializer.save(owner=Profile.get_profile(request.user), group=current_group, uuid=post_uuid)
                 [add_post_to_feed(str(post_uuid), str(group_member.uuid)) for group_member in current_group.members.all()]
->>>>>>> feed
 
     return Response(status=status.HTTP_201_CREATED)
