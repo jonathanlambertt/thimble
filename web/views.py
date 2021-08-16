a1ce5c49d63eebc58d7f12f6a75563b79e036f0c
jonathan <jonathanlambert247@gmail.com>
2021-08-16 09:24:08 -0700

    add view for sharing a post

--- web/views.py
+++ web/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+from posts.models import Post
+
 def home(request):
     return render(request, "web/index.html")
 
 def about(request):
     return render(request, "web/about.html")
+
+def post(request, post_id):
+    post = Post.get_by_uuid(post_id)
+    context = {"post": post}
+    return render(request, "web/post.html", context)
