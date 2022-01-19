06ae71bc73dcba60f18103455e6fa2cc8498f873
patrick <patrickmucsi@aol.com>
2022-01-18 17:15:39 -0800

    Made English sound better. Fixed notification implementation to allow more versitile notifying

--- groups/tasks.py
+++ groups/tasks.py
@@ -5,15 +5,14 @@ from .models import Group
 from users.RedisHelper import add_post_to_feed
 
 @shared_task
-def distribute_users_post_to_feeds(user_uuid, group_uuid, post_uuid):
+def post_and_notify_group(username, post_uuid, group_uuid):
     current_group = Group.get_by_uuid(group_uuid)
     [add_post_to_feed(post_uuid, str(group_member.uuid)) for group_member in current_group.members.all()]
-    notify_group_members_of_post.delay(group_uuid,user_uuid)
-
+    send_group_notification.delay(group_uuid, username, f'{username} posted in {current_group.name}.')
 
 @shared_task
-def notify_group_members_of_post(group_uuid, posting_user):
+def send_group_notification(group_uuid, username, notification_text):
     current_group = Group.get_by_uuid(group_uuid)
     for member in current_group.members.all():
-        if member.user.username != posting_user:
-            member.send_notification(f'{posting_user} posted in {current_group.name}')
+        if member.user.username != username:
+            member.send_notification(notification_text)
--- posts/views.py
+++ posts/views.py
@@ -9,7 +9,7 @@ from .serializers import *
 
 from users.models import Profile
 from groups.models import Group
-from groups.tasks import distribute_users_post_to_feeds
+from groups.tasks import post_and_notify_group
 from posts.models import Post
 
 import uuid
@@ -25,8 +25,8 @@ def create_post(request):
                 post_uuid = uuid.uuid4()
                 current_group = Group.get_by_uuid(request.data['group'])
                 post_serializer.save(owner=Profile.get_profile(request.user), group=current_group, uuid=post_uuid)
-                # Distributes and then notifies users of new post
-                distribute_users_post_to_feeds.delay(request.user.username, request.data['group'], str(post_uuid))
+                # Adds post to feeds of group members and then notifies group members
+                post_and_notify_group.delay(request.user.username, str(post_uuid), request.data['group'])
 
                 return Response(status=status.HTTP_201_CREATED)
     return Response(status=status.HTTP_400_BAD_REQUEST) # needs to eventually return data to say why it 400
